01365c98665dd4229a6e1a3c905cf90a19b8cf3e
Alex Turner <alexwilliamturner@gmail.com>
2020-05-22 15:24:42 +0100

    Init Project

---
+++ ClientApp/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray } from '@angular/forms'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+  title = 'Silxo Assessment';
+}
